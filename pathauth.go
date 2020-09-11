a39db1663447e517c6bb9b797514a45c6750532b
austin <austin@austins-MBP.fritz.box>
2020-09-11 12:50:12 +0200

    moved regex compile to plugin creation function, refactored

--- pathauth.go
+++ pathauth.go
@@ -5,27 +5,23 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 	"regexp"
-	
+	"strings"
 )
 
 // Config the plugin configuration.
 type Config struct {
 	Groups map[string][]string `json:"groups,omitempty"`
-	Paths []Path `json:"paths,omitempty"`
+	Paths  []Path              `json:"paths,omitempty"`
 }
-
 
 // Path holds one path vs user check
 type Path struct {
-	Regex string `json:"path,omitempty"`
-	Users []string `json:"users,omitempty"`
+	Regex  string   `json:"path,omitempty"`
+	Users  []string `json:"users,omitempty"`
 	Groups []string `json:"groups,omitempty"`
-	Public bool `json:"public,omitempty"`
+	Public bool     `json:"public,omitempty"`
 }
-
-
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
@@ -34,103 +30,89 @@ func CreateConfig() *Config {
 
 // PathAuth a PathAuth plugin.
 type PathAuth struct {
-	next     http.Handler
-	publicPaths []string
-	users	 map[string][]string
-	name     string
-}
-
-func find(source []string, value string) bool {
-    for _, item := range source {
-        if item == value {
-			fmt.Println(item)
-            return true
-        }
-    }
-    return false
+	next        http.Handler
+	publicPaths []*regexp.Regexp
+	users       map[string][]*regexp.Regexp
+	name        string
 }
 
 // New created a new PathAuth plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	fmt.Println(config.Groups)
-	publicPaths := []string{}
-	users := make(map[string][]string)
+	publicPaths := []*regexp.Regexp{}
+	users := make(map[string][]*regexp.Regexp)
 	// create map of user and regexs
 	for _, path := range config.Paths {
+		re := regexp.MustCompile(path.Regex)
 		if path.Public {
-			publicPaths = append(publicPaths, path.Regex)
+			publicPaths = append(publicPaths, re)
 		}
-		u := []string{}
+		u := make(map[string]bool)
 		for _, group := range path.Groups {
-			fmt.Println(config.Groups[group])
 			for _, user := range config.Groups[group] {
-				if find(u,user) == false {
-					u = append(u, user)
-				}
+				u[user] = true
 			}
 		}
 		for _, user := range path.Users {
-			if find(u,user) == false {
-					u = append(u, user)
-				}
-			}
-		for _, user := range u {
-			users[user] = append(users[user], path.Regex)
-		}	
+			u[user] = true
+		}
+		fmt.Println(u)
+		for user, _ := range u {
+			users[user] = append(users[user], re)
+		}
 	}
-	fmt.Println(users)
+	// fmt.Println(publicPaths, "\n", users)
 	return &PathAuth{
 		publicPaths: publicPaths,
-		users: users,
-		next:     next,
-		name:     name,
+		users:       users,
+		next:        next,
+		name:        name,
 	}, nil
 }
 
 func (a *PathAuth) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	authenticated := false
 	user := ""
 	// Read cookie
 	cookie, cerr := req.Cookie("_forward_auth")
 	if cerr != nil {
-		fmt.Printf("Can't find forward auth cookie :/\r\n")
+		fmt.Printf("No _forward_auth cookie\r\n")
 	} else {
 		user = strings.Split(cookie.Value, "|")[2]
 	}
 	basicAuthUser, _, ok := req.BasicAuth()
 	if !ok {
-		fmt.Printf("Can't find BasicAuth :/\r\n")
+		fmt.Printf("No BasicAuth\r\n")
 	} else {
 		user = basicAuthUser
 	}
 	if user == "" {
-		fmt.Printf("cannot determine current user :/\r\n")
+		fmt.Printf("cannot determine current user\r\n")
 		rw.WriteHeader(http.StatusForbidden)
-			return
+		return
 	}
-	currentPath := req.URL.EscapedPath()
-	for _, path := range a.users[user] {
-		fmt.Println("looking for ", path)
-		re := regexp.MustCompile(path)
-		if re.MatchString(currentPath) {
-			authenticated = true
-			break
-				}
-			}
-	// if not authenticated check public paths
-	if !authenticated {
-		for _, path := range a.publicPaths {
-			re := regexp.MustCompile(path)
-			if re.MatchString(currentPath) {
-				authenticated = true
-				break
-					}
-		}
-		if !authenticated {
-			rw.WriteHeader(http.StatusForbidden)
-			return
 
+	// loop regex's allowed by user
+
+	currentPath := req.URL.EscapedPath()
+	for _, regex := range a.users[user] {
+		if regex.MatchString(currentPath) {
+			a.next.ServeHTTP(rw, req)
+			return
 		}
 	}
-	a.next.ServeHTTP(rw, req)
+
+	// if not authenticated check public paths
+
+	for _, regex := range a.publicPaths {
+		if regex.MatchString(currentPath) {
+			a.next.ServeHTTP(rw, req)
+			return
+		}
+	}
+
+	// if still not authed then return status forbidden
+
+	rw.WriteHeader(http.StatusForbidden)
+	return
+
 }
