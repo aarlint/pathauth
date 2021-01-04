f411deffd0e6471df16dc92e3e263ffc6da60088
Austin Arlint <aarlint@gmail.com>
2021-01-04 13:06:58 +0100

    Update pathauth.go

--- pathauth.go
+++ pathauth.go
@@ -19,7 +19,7 @@ var (
 )
 
 func init() {
-	file, err := os.OpenFile("/var/log/pathauth.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile("/var/log/pathauth.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
